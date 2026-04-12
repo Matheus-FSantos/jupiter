447a01c6e0284afe8aa92d4c8177ab988cdc3ef9
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2026-04-11 23:00:17 -0300

    feat: add service discovery and configurations to jp-user service (#1)

---
+++ jp-service-discovery/src/test/java/io/github/matheus_fsantos/jupiter/jp_service_discovery/JpServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.matheus_fsantos.jupiter.jp_service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
