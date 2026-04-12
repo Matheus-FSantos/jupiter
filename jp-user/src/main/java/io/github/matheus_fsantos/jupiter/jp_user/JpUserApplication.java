c4c2d8d7d938b5ea2bca7fbd6cd8a47de13ca64a
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2026-04-11 23:02:09 -0300

    feat: add service discovery and configurations to jp-user service (#1) (#2)

--- jp-user/src/main/java/io/github/matheus_fsantos/jupiter/jp_user/JpUserApplication.java
+++ jp-user/src/main/java/io/github/matheus_fsantos/jupiter/jp_user/JpUserApplication.java
@@ -2,6 +2,7 @@ package io.github.matheus_fsantos.jupiter.jp_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class JpUserApplication {
