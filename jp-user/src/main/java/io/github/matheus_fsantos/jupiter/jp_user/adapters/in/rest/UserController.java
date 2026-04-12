fbd777161daf973e0d5265a75142a2dc48d6a0d7
Matheus Ferreira Santos <109228925+Matheus-FSantos@users.noreply.github.com>
2026-04-11 23:40:44 -0300

    feat/0002-add-cloud-gateway (#3)
    * feat: add service discovery and configurations to jp-user service (#1)
    
    * feat(load-balancer): add cloud gateway - w. load balancer and routing dist.

--- jp-user/src/main/java/io/github/matheus_fsantos/jupiter/jp_user/adapters/in/rest/UserController.java
+++ jp-user/src/main/java/io/github/matheus_fsantos/jupiter/jp_user/adapters/in/rest/UserController.java
@@ -10,10 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -21,6 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users", produces = {MediaType.APPLICATION_JSON_VALUE })
 public class UserController {
     private final SaveUserInputPort saveUserInputPort;
+
+    @GetMapping("/ping")
+    ResponseEntity<String> ping() {
+        return ResponseEntity.status(HttpStatus.OK).body("pong");
+    }
 
     @PostMapping
     ResponseEntity<Void> save(@Valid @RequestBody NewUserRequest newUserRequest) {
